701cee1f46e205da0df122c062582f545587f1fa
Camilo Gonzalez <cgs.camilogonzalez@gmail.com>
2015-07-09 23:24:31 -0400

    first commit

---
+++ src/deansList/overnightRobot/RobotMap.java
@@ -0,0 +1,12 @@
+package deansList.overnightRobot;
+
+/**
+ * 
+ * @author Camilo Gonzalez
+ *
+ */
+public class RobotMap {
+		
+	public static int LEFT_DRIVE_MOTOR = 0;
+	public static int RIGHT_DRIVE_MOTOR = 0;
+}
---
+++ src/deansList/overnightRobot/subsystems/DriveTrain.java
@@ -0,0 +1,29 @@
+package deansList.overnightRobot.subsystems;
+
+import deansList.overnightRobot.RobotMap;
+import edu.wpi.first.wpilibj.Talon;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ * 
+ * @author Camilo Gonzalez
+ *
+ */
+public class DriveTrain extends Subsystem {
+    
+	private Talon leftDrive = new Talon(RobotMap.LEFT_DRIVE_MOTOR);
+	private Talon rightDrive = new Talon(RobotMap.RIGHT_DRIVE_MOTOR);
+
+    public void initDefaultCommand() {
+        // Set the default command for a subsystem here.
+        //setDefaultCommand(new MySpecialCommand());
+    }
+    
+    public void setLeftPower(double pow) {
+    	this.leftDrive.set(pow);
+    }
+    
+    public void setRightPower(double pow) {
+    	this.rightDrive.set(pow);
+    }
+}
